0964d24777c70fd9990c8020cd2143a453e54187
Parth099 <parthnpatel715@gmail.com>
2022-07-02 21:34:41 -0400

    added svg ts support

---
+++ react-app-env.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="react-scripts" />
+
+//sigh
+declare module "*.svg" {
+    const content: any;
+    export default content;
+}
+declare module "*.png" {
+    const content: any;
+    export default content;
+}
